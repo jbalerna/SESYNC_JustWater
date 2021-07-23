d28a72b61767b0038e60fe2c29847029534dce69
jbalerna <jessicabalerna@gmail.com>
2021-07-23 11:00:42 -0400

    Updated mapping

--- add-census-data.R
+++ add-census-data.R
@@ -3,7 +3,7 @@ install.packages("tidycensus")
 library(tidycensus)
 census_api_key("f7a31bca866d690a768302cd1cea2e32346f2006", install=TRUE)
 
-#view in just 2005 
+#view in just 2005 - helps you see which codes you need to find income, pop size, etc.
 v2005 <- load_variables(2005, "acs1", cache = TRUE)
 v2005
 
@@ -16,19 +16,57 @@ md <- get_acs(geography = "county",
 
 md_sf<- st_as_sf(md) #has same coordinate system as nrrss data
 
-#double check crs
-st_crs(md_sf)
+md_sf_test <- st_sf(md, 
+                     crs=4269)
 
-ggplot(md_sf, aes(fill = estimate)) +
+st_crs(md_sf_test)
+                     
+                     
+
+#double check crs
+st_crs(md_sf_test)
+st_crs(md_nrrss)
+
+ggplot(md_sf_test, aes(fill = estimate)) +
   geom_sf() 
 
-md_nrrss<-st_filter(proj_location_cost_sf, md_sf) # proj_location_cost_sf made in All-nrrss-map.R
+md_nrrss<-st_filter(proj_location_cost_sf, md_sf_test) # proj_location_cost_sf made in All-nrrss-map.R
 
-mapview(md_nrrss["proj_cost"],
-        map.types = 'OpenStreetMap',
-        viewer.suppress=TRUE)
 
-ggplot(md_sf, 
+#Add some additional points for reference
+sesync <- st_sfc(
+  st_point(c(-76.503394, 38.976546)),
+  crs = st_crs(md_sf))
+
+oriole_park <- st_sfc(
+  st_point(c(-76.6216, 39.2839)),
+  crs = st_crs(md_sf))
+
+
+#plot plot plot!
+mapview(md_sf_test, 
+        map.types = "OpenStreetMap") +
+    mapview(md_nrrss)
+
+pal <- colorNumeric("PuBuGn", md_sf_test$estimate)
+
+leaflet() %>%
+  addTiles() %>%
+  addPolygons(data = md_sf_test,
+              fillColor = ~pal(estimate),
+              fillOpacity = 0.75,
+              weight=1) %>%
+  addLegend(pal = pal, values = md_sf_test$estimate, 
+            title = "Median Income",
+            position = "bottomleft") %>%
+  addCircleMarkers(data=md_nrrss,
+                   color="red",
+                   fillOpacity = 0.7,
+                   radius = 0.2)
+
+?addLegend
+
+ggplot(md_sf_test, 
        aes(fill = estimate/10000)) +
   geom_sf()  +
   labs(fill="Median Income/10000") +
@@ -46,13 +84,4 @@ ggplot(md_sf,
     x = -76.6216, y = 39.25, size = 3.5, colour = "white", font=2)
 
 
-?ggplot
 
-#Add some additional points for reference
-sesync <- st_sfc(
-  st_point(c(-76.503394, 38.976546)),
-  crs = st_crs(md_sf))
-
-oriole_park <- st_sfc(
-  st_point(c(-76.6216, 39.2839)),
-  crs = st_crs(md_sf))
